075efd29f3f6c378f403704495026858edf3b00f
Enrique Ruiz <eruiz@iespabloserrano.com>
2025-09-23 12:30:08 +0200

    Añadir saludo personalizado

--- Saludo.java
+++ Saludo.java
@@ -1,7 +1,24 @@
-public class Saludo {
+import java.util.Scanner;
+public class Hola_personalizado {
 
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+
+        do {
+            System.out.print("Introduce tu nombre: ");
+            nombre = sc.nextLine().trim(); // quitamos espacios en blanco al inicio y al final
+
+            if (nombre.isEmpty()) {
+                System.out.println("El nombre no puede estar vacío.");
+            }
+
+        } while (nombre.isEmpty());
+
+        // Saludo con el nombre introducido
+        System.out.println("¡Hola, " + nombre + "!");
         
-        System.out.println("Hola");
+        sc.close();
     }
 }
